3eedf66f6e2af98784502218f4c3bce8904771aa
Samiksha Khare <83221797+Samiksha1318@users.noreply.github.com>
2024-01-24 16:35:47 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,6 @@
+let moviesList = ['Fighter', 'Dunki', 'Salaar', 'Animal', 'Sam Bahadur'];
+let slots = ['10:30 AM', '01:25 PM', '04:00 PM', "09:00PM"];
+let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2'];
+exports.moviesList = moviesList;
+exports.slots = slots;
+exports.seats = seats;
